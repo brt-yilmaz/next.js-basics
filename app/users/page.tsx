b5c2c2168fefe1bc1d31334584a4244d84c51231
Berat Yilmaz <beratyilmaz3102@gmail.com>
2023-07-20 17:19:14 +0200

    feat(fetchUser) Add fetchUser

---
+++ app/users/page.tsx
@@ -0,0 +1,33 @@
+import getAllUser from '@/lib/getAllUser'
+import { Metadata } from 'next'
+import Link from 'next/link';
+import React from 'react'
+
+export const metadata: Metadata = {
+  title:'Users',
+}
+
+export default async function page() {
+  const usersData: Promise<User[]> = getAllUser();
+  
+  const users = await usersData ;
+  const content = (
+    <section>
+      <h2>
+        <Link href={'/'}>Back to Home</Link>
+      </h2>
+      <br/>
+      {users.map((user) => (
+        <>
+          <p key={user.id}>
+            <Link href={`/users/${user.id}`}>{user.name}</Link>
+          </p>
+          <br/>
+
+        </>
+      ))}
+    </section>
+  )
+
+  return content;
+}
